89811242e4a8e131cfde38bcb8bf29cc462043b4
Jaqueline Sodré <jaquelinesodre@live.com>
2026-09-02 15:37:01 -0300

    config: eslint | shadcn/ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    ...nextVitals,
+    ...nextTs,
+    // Override default ignores of eslint-config-next.
+    globalIgnores([
+        // Default ignores of eslint-config-next:
+        '.next/**',
+        'out/**',
+        'build/**',
+        'next-env.d.ts'
+    ]),
 
-export default eslintConfig;
+    {
+        rules: {
+            'padding-line-between-statements': [
+                'error',
+                { blankLine: 'always', prev: 'import', next: 'export' },
+                { blankLine: 'always', prev: 'import', next: 'function' },
+                { blankLine: 'always', prev: 'import', next: 'const' }
+            ],
+
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            'no-console': ['warn', { allow: ['warn', 'error'] }],
+            eqeqeq: ['error', 'always'],
+            'react/jsx-no-target-blank': 'error',
+            'react/jsx-key': 'error',
+            'no-duplicate-imports': 'error'
+        }
+    },
+
+    eslintConfigPrettier
+])
+
+export default eslintConfig
